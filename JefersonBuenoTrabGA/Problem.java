52189da5a32cb04dd755aa7dca0460c7e2db2021
jfbueno <buenojeferson@outlook.com>
2020-04-15 07:55:21 -0300

    Classe para resolução de problema usando hashtable

--- JefersonBuenoTrabGA/Problem.java
+++ JefersonBuenoTrabGA/Problem.java
@@ -1,15 +1,24 @@
 package JefersonBuenoTrabGA;
 
+import java.util.stream.Stream;
+
 public class Problem {
     public static void main(String[] args) {
+        if(args == null || args.length == 0) {
+            args = new String[] { "10", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        }
+
+        Integer[] intArgs = Stream.of(args)
+            .map(Integer::valueOf)
+            .toArray(Integer[]::new);
+
+        var sum = intArgs[0];
+        var inputArray = Stream.of(intArgs).skip(1).toArray(Integer[]::new);
         
-        var sum = 10;
-        var inputArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-
-
+        solveProblem(sum, inputArray);
     }
 
-    public static void solveProblem(int sum, int[] input) {
+    public static void solveProblem(int sum, Integer[] input) {
         var hashtable = new HashtableSeparateChaining<Integer>(16);
         for(var i: input) {
             hashtable.insert(Item.create(i));
@@ -18,6 +27,17 @@ public class Problem {
         System.out.printf("Procurando por pares que tenham como soma %d%n", sum);
         // todo: printar o array
 
-        
+        for (var item : input) {
+            var complement = sum - item;
+            if(complement < 0) continue;
+            
+            var searchResult = hashtable.search(complement);
+
+            if(searchResult != null)
+                System.out.printf("(%d, %d)%n", item, searchResult.getValue());
+        }
+
+        System.out.printf("%n*** Mostrando a Hashtable criada para resolver o problema ***%n");
+        hashtable.print();
     }
 }
