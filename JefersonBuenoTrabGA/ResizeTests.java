c03578b5714ea1cf252a6fc425677652ac71a565
jfbueno <buenojeferson@outlook.com>
2020-04-15 08:57:35 -0300

    Melhoria: usar args da linha de comando nos testes

--- JefersonBuenoTrabGA/ResizeTests.java
+++ JefersonBuenoTrabGA/ResizeTests.java
@@ -1,25 +1,56 @@
 package JefersonBuenoTrabGA;
 
+import java.util.function.Supplier;
 import java.util.stream.Stream;
+
+import JefersonBuenoTrabGA.HashtableOpenAddressing.ColisionSolutionStrategy;
 
 public class ResizeTests {
     public static void main(String[] args) {
-        var hashTable = new HashtableSeparateChaining<Integer>(4);
-
-        if(args == null || args.length == 0) {
-            args = new String[] { "1", "2", "3", "4", "5", "6" };
+        final boolean debug = false;
+        if(debug) {
+            args = new String [] { "auto", "openaddressing", "1", "2" };
         }
 
-        Integer[] intArgs = Stream.of(args)
+        if(args == null || args.length < 1) {
+            System.out.printf("%s%s%s%n", AnsiEscapeCodes.RED, 
+                "Este programa precisa de, no mínimo, dois argumentos da linha de comando para funcionar", AnsiEscapeCodes.RESET);
+            
+            return;
+        }
+
+        //Supplier<Stream<String>> argsSupplier = () -> Stream.of(args);
+
+        var mode = args[0];
+        var type = args[1];
+
+        Hashtable<Integer> hashtable = "openaddressing".equals(type)
+            ? new HashtableOpenAddressing<>(4, 3, ColisionSolutionStrategy.DOUBLE_HASHING)
+            : new HashtableSeparateChaining<>(4);
+
+        switch(mode) {
+            case "interactive":
+                return;
+            case "auto":
+                autoMode(hashtable, Stream.of(args).skip(2).toArray(String[]::new));
+                break;
+            default:
+                System.out.printf("Valor %s inválido, os argumentos aceitos são %s e %s", mode, "interactive", "auto");
+                return;
+        }
+    }
+
+    private static void autoMode(Hashtable<Integer> hashtable, String[] input) {
+        Integer[] intArgs = Stream.of(input)
             .map(Integer::valueOf)
             .toArray(Integer[]::new);
 
-        hashTable.print();
+        hashtable.print();
 
         for (var arg : intArgs) {
             System.out.printf("%nInserindo %d%n", arg);
-            hashTable.insert(Item.create(arg));
-            hashTable.print();
+            hashtable.insert(Item.create(arg));
+            hashtable.print();
         }
     }
 }
